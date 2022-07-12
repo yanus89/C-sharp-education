5b9af66d012e83e9c9280f6d415989611e0eb87b
Alexandr <shkiopu.a@gmail.com>
2022-07-12 23:39:09 +0300

    .\Example005_ConditionIfElse\

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "саша")
+{
+    Console.WriteLine("Ура, это же САША!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
